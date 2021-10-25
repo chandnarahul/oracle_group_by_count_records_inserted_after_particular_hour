42e8424ca8beae37810af3ef6d59e7d49e3d86c9
Rahul Chandna <chandnarahul@yahoo.com>
2021-10-25 21:23:16 +0200

    Create 002_add_data_to_table.sql

---
+++ 002_add_data_to_table.sql
@@ -0,0 +1,11 @@
+-- add some data to the table
+insert into message_archive (WHENRECEIVED, topic) values(TO_DATE('15-07-2020 17:02:44', 'DD-MM-YYYY hh24:mi:ss'),'topic1');
+insert into message_archive (WHENRECEIVED, topic) values(TO_DATE('15-07-2020 18:02:44', 'DD-MM-YYYY hh24:mi:ss'),'topic1');
+insert into message_archive (WHENRECEIVED, topic) values(TO_DATE('15-07-2020 20:02:44', 'DD-MM-YYYY hh24:mi:ss'),'topic1');
+insert into message_archive (WHENRECEIVED, topic) values(TO_DATE('15-07-2020 20:02:44', 'DD-MM-YYYY hh24:mi:ss'),'topic1');
+
+insert into message_archive (WHENRECEIVED, topic) values(TO_DATE('15-07-2020 21:02:44', 'DD-MM-YYYY hh24:mi:ss'),'topic2');
+insert into message_archive (WHENRECEIVED, topic) values(TO_DATE('15-07-2020 21:02:44', 'DD-MM-YYYY hh24:mi:ss'),'topic2');
+
+insert into message_archive (WHENRECEIVED, topic) values(TO_DATE('15-07-2020 22:02:44', 'DD-MM-YYYY hh24:mi:ss'),'topic3');
+insert into message_archive (WHENRECEIVED, topic) values(TO_DATE('15-07-2020 22:02:44', 'DD-MM-YYYY hh24:mi:ss'),'topic3');
